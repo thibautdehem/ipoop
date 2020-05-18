7149ed191ab25d6fa7a718c0bb45743fb91ccc84
thibaut dehem <thibaut.dehem@gmail.com>
2020-05-18 18:02:54 +0200

    added toilets in db

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 2020_05_18_141533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "toilets", force: :cascade do |t|
+    t.string "address"
+    t.string "style"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
