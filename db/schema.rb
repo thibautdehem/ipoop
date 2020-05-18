436850a8245cdd6038d4ccac8c846d4d42d44483
thibaut dehem <thibaut.dehem@gmail.com>
2020-05-18 17:54:32 +0200

    drop the DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_130509) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "toilets", force: :cascade do |t|
-    t.string "address"
-    t.string "style"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
