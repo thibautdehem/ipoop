b501905eb8534dedff6a6399ec387116478239d7
Pietro Fouarge <pietrofouarge@gmail.com>
2020-05-19 12:02:09 +0200

    faker added

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.6.6'
 
 gem 'devise'
 
+gem 'faker'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3'
 # Use postgresql as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+5.times do
+  toilet = Toilet.new(
+    description: Faker::Artist.name,
+    address: Faker::Address.full_address,,
+    category: ["turk", "italian", "french"].sample
+  )
+  toilet.save!
+end
