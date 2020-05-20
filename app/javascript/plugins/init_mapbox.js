5e88f73d386ad3463aa071bb2eea4baa1a44da91
thibaut dehem <thibaut.dehem@gmail.com>
2020-05-20 16:34:06 +0200

    need to fix single marker

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -12,11 +12,21 @@ const buildMap = () => {
 
 const addMarkersToMap = (map, markers) => {
   markers.forEach((marker) => {
+    const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
     new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
+      .setPopup(popup)
       .addTo(map);
   });
 };
+
+// const addMarkerToMap = (map, marker) => {
+//     const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+//     new mapboxgl.Marker()
+//       .setLngLat([ marker.lng, marker.lat ])
+//       .setPopup(popup)
+//       .addTo(map);
+// };
 
 const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
@@ -24,12 +34,24 @@ const fitMapToMarkers = (map, markers) => {
   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
 };
 
+// const fitMapToMarker = (map, marker) => {
+//   const bounds = new mapboxgl.LngLatBounds();
+//   marker => bounds.extend([ marker.lng, marker.lat ]);
+//   map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
+// };
+
 const initMapbox = () => {
   if (mapElement) {
     const map = buildMap();
+    console.log(map)
     const markers = JSON.parse(mapElement.dataset.markers);
+    // const marker = JSON.parse(mapElement.dataset.marker);
+    console.log(marker)
+    console.log(markers)
     addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
+    // addMarkerToMap(map, marker);
+    // fitMapToMarker(map, marker);
   }
 };
 
