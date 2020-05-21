1c26ab4d98258f722a1baba5d0dd6fc950aaba90
thibaut dehem <thibaut.dehem@gmail.com>
2020-05-21 06:28:11 +0200

    review booking WIP

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,5 @@
 class BookingsController < ApplicationController
- before_action :set_booking, only: [:show, :destroy]
+ before_action :set_booking, only: [:show, :destroy, :edit, :update]
  def index
         @bookings = Booking.all
     end
@@ -20,10 +20,18 @@ class BookingsController < ApplicationController
         authorize @booking
         @booking.toilet = @toilet
         if @booking.save!   # if the Model has a validates: presence
-            redirect_to toilet_path(@toilet)
+            redirect_to edit_toilet_booking_path(@booking) # /toilets/:toilet_id/bookings/:id/edit
           else
             render :new
           end
+    end
+
+    def edit
+    end
+
+    def update
+        @booking.update(booking_params)
+        redirect_to toilets_path
     end
 
     def destroy
@@ -34,10 +42,10 @@ class BookingsController < ApplicationController
     private
 
     def booking_params
-      params.require(:booking).permit(:arrivaltime, :pooptype)
+      params.require(:booking).permit(:arrivaltime, :pooptype, :review, :rating)
     end
 
-      def set_toilet
+      def set_booking
       @booking = Booking.find(params[:id])
       authorize @booking
     end
